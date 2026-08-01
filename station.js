ff73c48d0cec1fcf0149fb3808cea69cc6a54655
daniilmishkov-lab <daniil.mishkov@gmail.com>
2026-08-01 13:31:33 +0200

    Create station.js

---
+++ station.js
@@ -0,0 +1,81 @@
+const params =
+new URLSearchParams(window.location.search);
+
+
+const stationID =
+params.get("id");
+
+
+const station =
+stations[stationID];
+
+
+document.getElementById("title").innerHTML =
+station.title;
+
+
+document.getElementById("question").innerHTML =
+station.question;
+
+
+
+function checkAnswer(){
+
+
+let answer =
+document.getElementById("answer")
+.value
+.toLowerCase()
+.trim();
+
+
+
+if(answer === station.answer){
+
+
+localStorage.setItem(
+station.reward.id,
+"true"
+);
+
+
+
+document.getElementById("result").innerHTML =
+
+`
+
+<h2>🎉 Красава!</h2>
+
+<p>
+Вы нашли:
+</p>
+
+
+<img src="${station.reward.image}"
+width="120">
+
+
+<p>
+${station.reward.name}
+</p>
+
+
+<a href="map.html">
+🗺️ Zurück zur Karte
+</a>
+
+`;
+
+}
+
+else {
+
+
+document.getElementById("result").innerHTML =
+
+"❌ Лошара, подумай ещё.";
+
+}
+
+
+}
