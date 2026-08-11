61e9d7a34fa74c5ad92ee42188e547fa0737b148
daniilmishkov-lab <daniil.mishkov@gmail.com>
2026-08-11 20:27:17 +0200

    Update stations.js

--- stations.js
+++ stations.js
@@ -1,8 +1,8 @@
 const stations = {
 
-    bench: {
+    kopfhoerer: {
 
-        title: "Станция Лавка",
+        title: "Станция 1",
 
         question:
         "Соня прилегла по дороге домой отдохнуть и кайфово послушать музыку. Как зовут самого культового артиста того времени? Введите оригинальное написание его псевдонима. Подсказка: скрррр-скрррр",
@@ -21,9 +21,9 @@ const stations = {
         }
 
     },
-    church: {
+    phone: {
 
-        title: "Станция Церковь",
+        title: "Станция 2",
 
         question:
         "тестовый вопрос",
@@ -37,7 +37,112 @@ const stations = {
 
             name: "Мобила",
 
-            image: "img/iphone.jpg",
+            image: "img/iphone.jpeg",
+
+        }
+
+    },
+    glasses: {
+
+        title: "Станция 3",
+
+        question:
+        "тестовый вопрос",
+
+        answer:
+        "69",
+
+        reward: {
+
+            id: "glasses",
+
+            name: "Очки",
+
+            image: "img/glasses.png",
+
+        }
+
+    },
+    spinner: {
+
+        title: "Станция 4",
+
+        question:
+        "тестовый вопрос",
+
+        answer:
+        "69",
+
+        reward: {
+
+            id: "spinner",
+
+            name: "Спиннер",
+
+            image: "img/spinner.png",
+
+        }
+
+    },
+    rosathing: {
+
+        title: "Станция 5",
+
+        question:
+        "тестовый вопрос",
+
+        answer:
+        "69",
+
+        reward: {
+
+            id: "rosathing",
+
+            name: "Бальзам для губ",
+
+            image: "img/rosathing.png",
+
+        }
+
+    },
+    vans: {
+
+        title: "Станция 6",
+
+        question:
+        "тестовый вопрос",
+
+        answer:
+        "69",
+
+        reward: {
+
+            id: "vans",
+
+            name: "Кроссовок",
+
+            image: "img/vans.png",
+
+        }
+
+    },
+    arizona: {
+
+        title: "Станция 7",
+
+        question:
+        "тестовый вопрос",
+
+        answer:
+        "69",
+
+        reward: {
+
+            id: "tea",
+
+            name: "Аризона",
+
+            image: "img/arizona.png",
 
         }
 
